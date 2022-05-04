b22ea62fa369ac11eef14b609f04a9fd90c162c2
pedrokpdo <pedromotoehm@gmail.com>
2022-05-04 16:24:11 -0300

    atualizando filme principal

--- src/components/FeaturedMovies.js
+++ src/components/FeaturedMovies.js
@@ -1,10 +1,39 @@
 import React from 'react'
 import './FeatureMovies.css'
 
-export default () => {
+export default ({ item }) => {
+
+    console.log(item);
+
+    let firstDate = new Date(item.first_air_date)
+    let genres = []
+    for (let i in item.genres) {
+        genres.push(item.genres[i].name)
+    }
+
     return (
-        <div>
-            Filme em Destaque
-        </div>
+        <section className='featured' style={{
+            background: 'cover',
+            backgroundPosition: 'center',
+            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+        }}>
+            <div className='featured--vertical'>
+                <div className='featured--horizontal'>
+                    <div className='featured--name'>{item.original_name}</div>
+                    <div className='featured--info'>
+                        <div className='featured--points'>{item.vote_average}pontos</div>
+                        <div className='featured--year'>{firstDate.getFullYear()}</div>
+                        <div className='featured--popularidade'>{item.popularity}Popularidades</div>
+                    </div>
+                    <div className='featured--description'>{item.overview}Popularidades</div>
+                    <div className='featured--buttons'>
+                        <a href='/' >assistir</a>
+                        <a href='/' >+ minha lista</a>
+                    </div>
+                    <div className='featured--genres'><strong>Generos:</strong>{genres}</div>
+
+                </div>
+            </div>
+        </section>
     )
 }
