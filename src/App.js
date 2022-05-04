00719201ba077f6a6a675f29597ed0eace66ec2e
pedrokpdo <pedromotoehm@gmail.com>
2022-05-04 11:42:34 -0300

    pegando dados da api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
 import './App.css';
+import Tmdb from './Tmdb';
 
 function App() {
+
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList()
+      console.log(list);
+    }
+
+    loadAll()
+  },[])
+
   return (
     <div className="App">
       <h1>ola mundo</h1>
